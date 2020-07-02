37e8f3a5566d917bda8c214d7db411794af640e1
bicknoddy <64910363+bicknoddy@users.noreply.github.com>
2020-07-02 16:02:26 -0400

    working toward usable actions
    Starting to give each gameobject values that tie to a specific action and will either be locked or unlocked.
    
    Just started the process.

---
+++ Miles/Assets/ActionDetails.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionDetails : MonoBehaviour
+{
+  public string title;
+  public int reqDeckPwr;
+}
--- Miles/Assets/Scripts/PlayerMovement.cs
+++ Miles/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,13 @@ public class PlayerMovement : MonoBehaviour
 
   Transform player;
   bool isTouchingAction;
+  BoxCollider2D area;
   int speed = 500;
+  GameObject currentAction;
 
   void Start()
   {
+    area = GameObject.Find("GameBackground").GetComponent<BoxCollider2D>();
     player = GetComponent<Transform>();
   }
 
@@ -38,8 +41,13 @@ public class PlayerMovement : MonoBehaviour
 
     if(Input.GetKey(KeyCode.Space) && isTouchingAction)
     {
-      Debug.Log("run action");
+
     }
+
+    Vector3 clampedPosition = player.position;
+    clampedPosition.x = Mathf.Clamp(clampedPosition.x, area.bounds.min.x + 92, area.bounds.max.x - 92);
+    clampedPosition.y = Mathf.Clamp(clampedPosition.y, area.bounds.min.y + 92, area.bounds.max.y - 92);
+    player.position = clampedPosition;   
   }
 
   void OnTriggerEnter2D(Collider2D other)
@@ -47,14 +55,7 @@ public class PlayerMovement : MonoBehaviour
     if(other.tag == "Action")
     {
       isTouchingAction = true;
-    }
-  }
-
-  void OnTriggerExit2D(Collider2D other)
-  {
-    if(other.tag == "Background")
-    {
-      speed = -speed;
+      currentAction = other.gameObject;
     }
   }
   
