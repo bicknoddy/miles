b4c243f0f3a25496d11ec4a87c578bcd8e78838f
bicknoddy <64910363+bicknoddy@users.noreply.github.com>
2020-07-10 09:59:00 -0400

    Beginning Steps in 3D
    Camera stuff, collision stuff, playing with lighting.
    
    Simple set up, ready for the content.

---
+++ miles_test/Assets/ChangeMatColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMatColor : MonoBehaviour
+{
+  [SerializeField]
+  private Color color;
+
+  private void Awake()
+  {
+    gameObject.GetComponent<MeshRenderer>().material.color = color;
+  }
+}
---
+++ miles_test/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  private Rigidbody player;
+  private int speed = 5;
+  private List<Collider> touchWalls = new List<Collider>();
+
+  private void Awake()
+  {
+    player = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate()
+  {
+    float x = Input.GetAxisRaw("Horizontal");
+    float z = Input.GetAxisRaw("Vertical");
+
+    player.position += z * transform.forward * Time.deltaTime * speed;
+    player.position += x * transform.right * Time.deltaTime * speed;
+
+    if(touchWalls.Count >= 2)
+    {
+      if(x != 0 && z != 0)
+      {
+        speed = 1;
+      }
+    }
+    else
+    {
+      speed = 5;
+    }
+  }
+
+  private void OnCollisionEnter(Collision other)
+  {
+    if(other.collider.tag == "Bounds")
+    {
+    touchWalls.Add(other.collider);
+    }
+  }
+
+  private void OnCollisionExit(Collision other)
+  {
+    touchWalls.Remove(other.collider);
+  }
+
+}
