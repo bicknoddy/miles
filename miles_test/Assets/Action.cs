03ad80359073cc744e45f074564256cf8b0252f2
bicknoddy <64910363+bicknoddy@users.noreply.github.com>
2020-07-12 20:24:57 -0400

    Starting to attach script to actions
    just started that process

---
+++ miles_test/Assets/Action.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+  string name;
+  bool isUnlocked = false;
+
+  void checkReq()
+  {
+    
+  }
+}
---
+++ miles_test/Assets/ActionStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionStart : MonoBehaviour
+{
+  private void OnTriggerStay(Collider other)
+  {
+    if(other.tag == "Action")
+    {
+      if(Input.GetKey(KeyCode.Space))
+      {
+        StartAction();
+      }
+    }
+  }
+
+  private void StartAction()
+  {
+    
+  }
+}
